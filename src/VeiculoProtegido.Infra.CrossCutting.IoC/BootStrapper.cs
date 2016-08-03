d9d2405db5e27ebf104c43f3256808d6db84ec13
ClaudineiMonteiro <claudinei.monteiro@outlook.com>
2016-08-03 16:46:15 -0300

    [Add] Inicio do tratamento de injeção de dependência

---
+++ src/VeiculoProtegido.Infra.CrossCutting.IoC/BootStrapper.cs
@@ -0,0 +1,15 @@
+﻿using SimpleInjector;
+using VeiculoProtegido.Domain.Interfaces.Service;
+using VeiculoProtegido.Domain.Services;
+
+namespace VeiculoProtegido.Infra.CrossCutting.IoC
+{
+	public class BootStrapper
+	{
+		public static void RegisterServices(Container container)
+		{
+			//Domain
+			container.RegisterPerWebRequest<IEmpresaService, EmpresaService>();
+		}
+	}
+}
