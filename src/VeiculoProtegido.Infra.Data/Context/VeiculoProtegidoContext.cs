523f26b361c68aefc90e485600b66d5908f472f8
ClaudineiMonteiro <claudinei.monteiro@outlook.com>
2016-08-30 14:51:55 -0300

    [add] Contexto de banco de dados do EF

---
+++ src/VeiculoProtegido.Infra.Data/Context/VeiculoProtegidoContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+
+namespace VeiculoProtegido.Infra.Data.Context
+{
+	public class VeiculoProtegidoContext : DbContext
+	{
+		public VeiculoProtegidoContext()
+			: base("DefaultConnection")
+		{
+		}
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+			modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+			modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+			modelBuilder.Properties()
+				.Where(p => p.Name == string.Format("{0}Id", p.ReflectedType.Name))
+				.Configure(p => p.IsKey());
+
+			modelBuilder.Properties<string>()
+				.Configure(p => p.HasColumnType("varchar"));
+
+			modelBuilder.Properties<string>()
+				.Configure(p => p.HasMaxLength(100));
+
+			base.OnModelCreating(modelBuilder);
+		}
+
+		public override int SaveChanges()
+		{
+			foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataHoraUltimaAlteracao") != null && entry.Entity.GetType().GetProperty("DataHoraUltimaAlteracaoDataHoraCadastro") != null))
+			{
+				if (entry.State == EntityState.Added)
+				{
+					entry.Property("DataHoraUltimaAlteracao").CurrentValue = DateTime.Now;
+				}
+
+				if (entry.State == EntityState.Modified)
+				{
+					entry.Property("DataHoraUltimaAlteracao").IsModified = false;
+				}
+			}
+
+			foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataHoraCadastro") != null))
+			{
+				if (entry.State == EntityState.Added)
+				{
+					entry.Property("DataHoraCadastro").CurrentValue = DateTime.Now;
+				}
+
+				if (entry.State == EntityState.Modified)
+				{
+					entry.Property("DataHoraCadastro").IsModified = false;
+				}
+			}
+			return base.SaveChanges();
+		}
+	}
+}
---
+++ src/VeiculoProtegido.Infra.Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace VeiculoProtegido.Infra.Data.Interfaces
+{
+	public interface IUnitOfWork
+	{
+		void BeginTransaction();
+		void Commit();
+	}
+}
