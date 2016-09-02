b60ba4bf0a21ac7f969862e85185955884c9c45e
ClaudineiMonteiro <claudinei.monteiro@outlook.com>
2016-09-02 09:56:20 -0300

    [add] Projeto MVC

---
+++ src/VeiculoProtegido.UI.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VeiculoProtegido.UI.Mvc.Startup))]
+namespace VeiculoProtegido.UI.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
