7ea31b4929e24da1d2a922dbe5541bc89bed59f4
ClaudineiMonteiro <claudinei.monteiro@outlook.com>
2016-09-05 18:09:08 -0300

    [add] Configuração do mapeamento das models

---
+++ src/VeiculoProtegido.Application/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace VeiculoProtegido.Application.AutoMapper
+{
+	public class AutoMapperConfig
+	{
+		public static void RegisterMappins()
+		{
+			Mapper.Initialize(x =>
+			{
+				x.AddProfile<DomainToViewModelMappingProfile>();
+				x.AddProfile<ViewModelToDomainMappingProfile>();
+			});
+		}
+	}
+}
