fd644dd65bf31734c505ac2999920fb9d4770da3
ClaudineiMonteiro <claudinei.monteiro@outlook.com>
2016-09-05 10:25:04 -0300

    [add] Consulta de carros

---
+++ src/VeiculoProtegido.UI.Mvc/Controllers/FipeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VeiculoProtegido.UI.Mvc.Controllers
+{
+    public class FipeController : Controller
+    {
+        // GET: Fipe
+        public ActionResult Carro()
+        {
+            return View();
+        }
+    }
+}
