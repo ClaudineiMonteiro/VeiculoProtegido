599ed5acba2ae5562e9499d63f6c19ae5d573f0f
ClaudineiMonteiro <claudinei.monteiro@outlook.com>
2016-09-28 12:01:15 -0300

    [add] Repositorio de Faixas de Tabela de Precos

---
+++ src/VeiculoProtegido.Infra.Data/Repository/FaixaTabelaPrecoRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using VeiculoProtegido.Domain.Entities;
+using VeiculoProtegido.Domain.Interfaces.Repository;
+using VeiculoProtegido.Infra.Data.Context;
+
+namespace VeiculoProtegido.Infra.Data.Repository
+{
+	public class FaixaTabelaPrecoRepository : Repository<FaixaTabelaPreco>, IFaixaTabelaPrecoRepository
+	{
+		#region Builders
+		public FaixaTabelaPrecoRepository(VeiculoProtegidoContext context) : base(context) {}
+		#endregion
+
+		public FaixaTabelaPreco GetByPlano(byte planoId)
+		{
+			return Search(s => s.PlanoId.Equals(planoId)).FirstOrDefault();
+		}
+
+		public FaixaTabelaPreco GetByValueRange(decimal valor)
+		{
+			return Search(s => s.ValorDe >= valor && s.ValorAte <= valor).FirstOrDefault();
+		}
+	}
+}
