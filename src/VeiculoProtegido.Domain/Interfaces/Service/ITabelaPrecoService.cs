76cc4849d38b3ebb645e5ecc33b2861899eca394
ClaudineiMonteiro <claudinei.monteiro@outlook.com>
2016-09-28 17:26:11 -0300

    [add] Interface de servico da Tabela de Preco

---
+++ src/VeiculoProtegido.Domain/Interfaces/Service/ITabelaPrecoService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using VeiculoProtegido.Domain.Entities;
+
+namespace VeiculoProtegido.Domain.Interfaces.Service
+{
+	public interface ITabelaPrecoService : IDisposable
+	{
+		TabelaPreco Add(TabelaPreco tabelaPreco);
+		TabelaPreco GetById(Guid id);
+		TabelaPreco GetByDescricao(string descricao);
+		TabelaPreco GetByTipoFIPE(byte tipoFIPE);
+		IEnumerable<TabelaPreco> GetAll();
+		TabelaPreco Update(TabelaPreco tabelaPreco);
+		void Remove(Guid id);
+		FaixaTabelaPreco AddFaixaTabelaPreco(FaixaTabelaPreco faixaTabelaPreco);
+		FaixaTabelaPreco UpdateFaixaTabelaPreco(FaixaTabelaPreco faixaTabelaPreco);
+		FaixaTabelaPreco GetByFaixaTabelaPrecoId(Guid id);
+		FaixaTabelaPreco GetByPlanoId(byte planoId);
+		void RemoveFaixaTabelaPreco(Guid id);
+	}
+}
