928157f8996ba329973648c77669c568b08ed793
ClaudineiMonteiro <claudinei.monteiro@outlook.com>
2016-09-29 15:22:55 -0300

    [add] Interface Tabela de preco em application

---
+++ src/VeiculoProtegido.Application/Interfaces/ITabelaPrecoAppService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using VeiculoProtegido.Application.ViewModels;
+
+namespace VeiculoProtegido.Application.Interfaces
+{
+	public interface ITabelaPrecoAppService : IDisposable
+	{
+		TabelaPrecoFaixaTabelaPrecoViewModel Add(TabelaPrecoFaixaTabelaPrecoViewModel tabelaPrecoFaixaTabelaPrecoViewModel);
+		TabelaPrecoViewModel GetById(Guid id);
+		TabelaPrecoViewModel GetByTipoFIPE(byte tipoFipe);
+		TabelaPrecoViewModel GetByDescricao(string descricao);
+		IEnumerable<TabelaPrecoViewModel> GetAll();
+		TabelaPrecoViewModel Update(TabelaPrecoViewModel tabelaPrecoViewModel);
+		void Remove(Guid id);
+		FaixaTabelaPrecoViewModel AddFaixaTabelaPreco(FaixaTabelaPrecoViewModel faixaTabelaPrecoViewModel);
+		FaixaTabelaPrecoViewModel UpdateFaixaTabelaPreco(FaixaTabelaPrecoViewModel faixaTabelaPrecoViewModel);
+		FaixaTabelaPrecoViewModel GetByIdFaixaTabelaPreco(Guid id);
+		void RemoveFaixaTabelaPreco(Guid id);
+	}
+}
