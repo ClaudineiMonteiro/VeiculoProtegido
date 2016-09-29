3bfcde7505c1741e8bf45fcf6cbb4baec46b947a
ClaudineiMonteiro <claudinei.monteiro@outlook.com>
2016-09-29 15:28:43 -0300

    [add] Classe de ApplicationService

---
+++ src/VeiculoProtegido.Application/ApplicationService.cs
@@ -0,0 +1,30 @@
+﻿using VeiculoProtegido.Infra.Data.Interfaces;
+
+namespace VeiculoProtegido.Application
+{
+	public class ApplicationService
+	{
+		#region Attribuilts
+		private readonly IUnitOfWork _uow;
+		#endregion
+
+		#region Builders
+		public ApplicationService(IUnitOfWork uow)
+		{
+			_uow = uow;
+		}
+		#endregion
+
+		#region Methods
+		public void BeginTransaction()
+		{
+			_uow.BeginTransaction();
+		}
+
+		public void Commit()
+		{
+			_uow.Commit();
+		}
+		#endregion
+	}
+}
