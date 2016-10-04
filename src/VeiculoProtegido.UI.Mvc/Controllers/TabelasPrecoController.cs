a08cb01c3d6458444b9d60f64d6e70e35e12d053
ClaudineiMonteiro <claudinei.monteiro@outlook.com>
2016-10-04 15:19:49 -0300

    [change] Controller de Tabelas de Preco

--- src/VeiculoProtegido.UI.Mvc/Controllers/TabelasPrecoController.cs
+++ src/VeiculoProtegido.UI.Mvc/Controllers/TabelasPrecoController.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
+﻿using Microsoft.Ajax.Utilities;
+using System;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using VeiculoProtegido.Application.Interfaces;
 using VeiculoProtegido.Application.ViewModels;
-using VeiculoProtegido.UI.Mvc.Models;
 
 namespace VeiculoProtegido.UI.Mvc.Controllers
 {
-    public class TabelasPrecoController : Controller
+	public class TabelasPrecoController : Controller
     {
 		private readonly ITabelaPrecoAppService _tabelaPrecoAppService;
 
@@ -53,83 +48,93 @@ namespace VeiculoProtegido.UI.Mvc.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "TabelaPrecoId,Descricao,TipoFipe,DataHoraCadastro,DataHoraUltimaAlteracao")] TabelaPrecoViewModel tabelaPrecoViewModel)
-        {
-            if (ModelState.IsValid)
-            {
-                tabelaPrecoViewModel.TabelaPrecoId = Guid.NewGuid();
-                db.TabelaPrecoViewModels.Add(tabelaPrecoViewModel);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+		public ActionResult Create(TabelaPrecoFaixaTabelaPrecoViewModel tabelaPrecoFaixaTabelaPrecoViewModel)
+		{
+		if (ModelState.IsValid)
+		{
+				tabelaPrecoFaixaTabelaPrecoViewModel = _tabelaPrecoAppService.Add(tabelaPrecoFaixaTabelaPrecoViewModel);
+				if (!tabelaPrecoFaixaTabelaPrecoViewModel.ValidationResult.IsValid)
+				{
+					foreach (var erro in tabelaPrecoFaixaTabelaPrecoViewModel.ValidationResult.Erros)
+					{
+						ModelState.AddModelError(string.Empty, erro.Message);
+					}
+					return View(tabelaPrecoFaixaTabelaPrecoViewModel);
+				}
 
-            return View(tabelaPrecoViewModel);
-        }
+				if (!tabelaPrecoFaixaTabelaPrecoViewModel.ValidationResult.Message.IsNullOrWhiteSpace())
+				{
+					ViewBag.Sucesso = tabelaPrecoFaixaTabelaPrecoViewModel.ValidationResult.Message;
+					return View(tabelaPrecoFaixaTabelaPrecoViewModel);
+				}
+				return RedirectToAction("Index");
+			}
+			return View(tabelaPrecoFaixaTabelaPrecoViewModel);
+		}
 
-        // GET: TabelasPreco/Edit/5
-        public ActionResult Edit(Guid? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            TabelaPrecoViewModel tabelaPrecoViewModel = db.TabelaPrecoViewModels.Find(id);
-            if (tabelaPrecoViewModel == null)
-            {
-                return HttpNotFound();
-            }
-            return View(tabelaPrecoViewModel);
-        }
+		// GET: TabelasPreco/Edit/5
+		public ActionResult Edit(Guid? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
 
-        // POST: TabelasPreco/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "TabelaPrecoId,Descricao,TipoFipe,DataHoraCadastro,DataHoraUltimaAlteracao")] TabelaPrecoViewModel tabelaPrecoViewModel)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(tabelaPrecoViewModel).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(tabelaPrecoViewModel);
-        }
+			TabelaPrecoViewModel tabelaPrecoViewModel = _tabelaPrecoAppService.GetById(id.Value);
+			if (tabelaPrecoViewModel == null)
+			{
+				return HttpNotFound();
+			}
+			ViewBag.TabelaPrecoId = id;
+			return View(tabelaPrecoViewModel);
+		}
 
-        // GET: TabelasPreco/Delete/5
-        public ActionResult Delete(Guid? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            TabelaPrecoViewModel tabelaPrecoViewModel = db.TabelaPrecoViewModels.Find(id);
-            if (tabelaPrecoViewModel == null)
-            {
-                return HttpNotFound();
-            }
-            return View(tabelaPrecoViewModel);
-        }
+		// POST: TabelasPreco/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(TabelaPrecoViewModel tabelaPrecoViewModel)
+		{
+			if (ModelState.IsValid)
+			{
+				_tabelaPrecoAppService.Update(tabelaPrecoViewModel);
+				return RedirectToAction("Index");
+			}
+			return View(tabelaPrecoViewModel);
+		}
 
-        // POST: TabelasPreco/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(Guid id)
-        {
-            TabelaPrecoViewModel tabelaPrecoViewModel = db.TabelaPrecoViewModels.Find(id);
-            db.TabelaPrecoViewModels.Remove(tabelaPrecoViewModel);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+		// GET: TabelasPreco/Delete/5
+		public ActionResult Delete(Guid? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			TabelaPrecoViewModel tabelaPrecoViewModel = _tabelaPrecoAppService.GetById(id.Value);
+			if (tabelaPrecoViewModel == null)
+			{
+				return HttpNotFound();
+			}
+			return View(tabelaPrecoViewModel);
+		}
+
+		// POST: TabelasPreco/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(Guid id)
+		{
+			_tabelaPrecoAppService.Remove(id);
+			return RedirectToAction("Index");
+		}
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+			if (disposing)
+			{
+				_tabelaPrecoAppService.Dispose();
+			}
+			base.Dispose(disposing);
+		}
     }
 }
