0fd26e9b5ae1fe62b5addaf9872d60c7a9b4ac55
ClaudineiMonteiro <claudinei.monteiro@outlook.com>
2016-10-19 13:54:17 -0200

    [change] Finalizado funcionamento da gravação da tabela de preço.

--- src/VeiculoProtegido.Application/ViewModels/TabelaPrecoFaixaTabelaPrecoViewModel.cs
+++ src/VeiculoProtegido.Application/ViewModels/TabelaPrecoFaixaTabelaPrecoViewModel.cs
@@ -15,11 +15,13 @@ namespace VeiculoProtegido.Application.ViewModels
 		[Key]
 		public Guid TabelaPrecoId { get; set; }
 
+		[Display(Name = "Descrição")]
 		[Required(ErrorMessage = "Preencha o campo Descrição")]
 		[MaxLength(100, ErrorMessage = "Máximo {0} caracteres")]
 		[MinLength(2, ErrorMessage = "Mínimo {0} caracteres")]
 		public string Descricao { get; set; }
 
+		[Display(Name = "Tipo da Tabela Fipe")]
 		[Required(ErrorMessage = "Preencha o campo Tipo FIPE")]
 		public int TipoFipe { get; set; }
 		[ScaffoldColumn(false)]
@@ -32,17 +34,30 @@ namespace VeiculoProtegido.Application.ViewModels
 		//Faixas de Tabela de Preço
 		[Key]
 		public Guid FaixatabelaPrecoId { get; set; }
+
+		[Display(Name = "Plano")]
 		[Required(ErrorMessage = "Preencha o campo Plano")]
 		//[MaxLength(3, ErrorMessage = "Máximo {0} caracteres")]
 		public int PlanoId { get; set; }
+
+		[Display(Name = "Valor de")]
 		[Required(ErrorMessage = "Preencha o campo Valor De")]
 		public decimal ValorDe { get; set; }
+
+		[Display(Name = "Valor até")]
 		[Required(ErrorMessage = "Preencha o campo Valor Até")]
 		public decimal ValorAte { get; set; }
+
+
+		[Display(Name = "Valor mensal")]
 		[Required(ErrorMessage = "Preencha o campo Valor Mensalidade")]
 		public decimal ValorMensal { get; set; }
+
+		[Display(Name = "Valor participação no evento")]
 		[Required(ErrorMessage = "Preencha o campo Valor Participação")]
 		public decimal ValorParticipacaoEvento { get; set; }
+
+		[Display(Name = "Valor da adesão")]
 		[Required(ErrorMessage = "Preencha o campo Valor de Adesão")]
 		public decimal ValorAdesao { get; set; }
 	}
